8574e91968ee57f2a106298622fb9e5887db88da
BxsedSosa <donovanxabad@gmail.com>
2026-01-07 12:28:23 -0800

    made a basic digital clock

--- digital-clock/main.c
+++ digital-clock/main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
 
 int main() {
-  printf("testing\n");
+  while (1) {
+    system("clear");
+    time_t now = time(NULL);
+    struct tm *local = localtime(&now);
+    printf("Time: %02d:%02d:%02d\n", local->tm_hour, local->tm_min,
+           local->tm_sec);
+    sleep(1);
+  }
   return 0;
 }
