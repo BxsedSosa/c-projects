66b36642d491243e4c5d497aa45b2121d51a8afe
BxsedSosa <donovanxabad@gmail.com>
2026-01-08 08:10:51 -0800

    properly centered to screen and increments again

--- stop-watch/main.c
+++ stop-watch/main.c
@@ -5,17 +5,20 @@
 #include <unistd.h>
 
 void stop_watch() {
+  char time_format[] = "%02d:%02d:%02d\n";
   time_t curr_time = 0;
 
   initscr();
 
-  struct tm *filtered_time = localtime(&curr_time);
-  sleep(1);
-  clear();
-  curr_time++;
-  mvprintw(LINES / 2, COLS / 2, "%02d:%02d:%02d\n", filtered_time->tm_hour - 16,
-           filtered_time->tm_min, filtered_time->tm_sec);
-  refresh();
+  while (1) {
+    struct tm *filtered_time = localtime(&curr_time);
+    sleep(1);
+    clear();
+    curr_time++;
+    mvprintw(LINES / 2, COLS / 2 - 3, time_format, filtered_time->tm_hour - 16,
+             filtered_time->tm_min, filtered_time->tm_sec);
+    refresh();
+  }
 
   getch();
   endwin();
