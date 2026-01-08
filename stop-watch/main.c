ecbfa1b8c05e549870eedd7c921e16becd573e2b
BxsedSosa <donovanxabad@gmail.com>
2026-01-08 07:59:22 -0800

    got to center the screen with the time but the increment is now broken

--- stop-watch/main.c
+++ stop-watch/main.c
@@ -1,3 +1,4 @@
+#include <curses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -6,14 +7,18 @@
 void stop_watch() {
   time_t curr_time = 0;
 
-  while (1) {
-    struct tm *filtered_time = localtime(&curr_time);
-    sleep(1);
-    system("clear");
-    curr_time++;
-    printf("%02d:%02d:%02d\n", filtered_time->tm_hour - 16,
+  initscr();
+
+  struct tm *filtered_time = localtime(&curr_time);
+  sleep(1);
+  clear();
+  curr_time++;
+  mvprintw(LINES / 2, COLS / 2, "%02d:%02d:%02d\n", filtered_time->tm_hour - 16,
            filtered_time->tm_min, filtered_time->tm_sec);
-  }
+  refresh();
+
+  getch();
+  endwin();
 }
 
 int main() {
