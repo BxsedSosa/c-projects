79c907e0f32d084ada509fad29b2079908fb63f9
BxsedSosa <donovanxabad@gmail.com>
2026-01-08 07:21:39 -0800

    working stop watch

--- stop-watch/main.c
+++ stop-watch/main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <unistd.h>
+
+void stop_watch() {
+  time_t curr_time = 0;
+
+  while (1) {
+    struct tm *filtered_time = localtime(&curr_time);
+    sleep(1);
+    system("clear");
+    curr_time++;
+    printf("%02d:%02d:%02d\n", filtered_time->tm_hour - 16,
+           filtered_time->tm_min, filtered_time->tm_sec);
+  }
+}
 
 int main() {
-  printf("Hello");
+  stop_watch();
   return 0;
 }
