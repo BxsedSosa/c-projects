21265a89983d372692e9359cc0660e06d4a5f2c5
BxsedSosa <donovanxabad@gmail.com>
2026-01-08 16:32:41 -0800

    appends elipsed time to a text file

--- stop-watch/main.c
+++ stop-watch/main.c
@@ -16,7 +16,7 @@ time_t stop_watch(char format[]) {
   timeout(1000);
   mvprintw(LINES / 2, COLS / 2 - 3, format, 00, 00, 00);
   noecho();
-  for (time_t time;; time++) {
+  for (time_t time = 0;; time++) {
     int c;
     struct tm *filtered_time = localtime(&time);
     mvprintw(LINES / 2, COLS / 2 - 3, format, filtered_time->tm_hour - 16,
@@ -32,12 +32,20 @@ time_t stop_watch(char format[]) {
   return saved_time;
 }
 
+void save_time(time_t saved_time, char format[]) {
+  FILE *fptr;
+  struct tm *filtered_time = localtime(&saved_time);
+
+  fptr = fopen("timestamps.txt", "a");
+  fprintf(fptr, format, filtered_time->tm_hour - 16, filtered_time->tm_min,
+          filtered_time->tm_sec);
+  fclose(fptr);
+  printf("Saved Time\n");
+}
+
 int main() {
   char time_format[] = "%02d:%02d:%02d\n";
   time_t saved_time = stop_watch(time_format);
-  struct tm *filter_saved_time = localtime(&saved_time);
-
-  printf(time_format, filter_saved_time->tm_hour - 16,
-         filter_saved_time->tm_min, filter_saved_time->tm_sec);
+  save_time(saved_time, time_format);
   return 0;
 }
