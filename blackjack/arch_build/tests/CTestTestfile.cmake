0e3a17007535921918b5a645244296c2970177a1
BxsedSosa <donovanxabad@gmail.com>
2026-01-15 19:16:49 -0800

    deleted testing folder

--- blackjack/arch_build/tests/CTestTestfile.cmake
+++ blackjack/arch_build/tests/CTestTestfile.cmake
@@ -5,4 +5,4 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 add_test([=[test_app]=] "/home/x/workspace/c-projects/blackjack/arch_build/tests/test_app")
-set_tests_properties([=[test_app]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/x/workspace/c-projects/blackjack/tests/CMakeLists.txt;3;add_test;/home/x/workspace/c-projects/blackjack/tests/CMakeLists.txt;0;")
+set_tests_properties([=[test_app]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/x/workspace/c-projects/blackjack/tests/CMakeLists.txt;7;add_test;/home/x/workspace/c-projects/blackjack/tests/CMakeLists.txt;0;")
