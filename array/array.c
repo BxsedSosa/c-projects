0fba7587414ac8ec7ead3809cab145b4413ed930
BxsedSosa <donovanxabad@gmail.com>
2026-01-31 06:15:39 -0800

    added couple other features

--- array/array.c
+++ array/array.c
@@ -12,14 +12,47 @@ int main() {
   printArray();
 
   printf("GOT VALUE: %s\n", get(1));
+
+  pop();
+
+  add("hello world");
+  printArray();
+  erase(1);
+
+  add("another test");
+  printArray();
+
+  insert(1, "cup");
+  printArray();
+
+  replace(0, "keyboard");
+  printArray();
   return 0;
 }
 
 void add(char *p) { arr[currEnd++] = p; }
-char *get(int index) { return arr[index]; }
-
+void replace(int idx, char *p) { arr[idx] = p; }
+void insert(int idx, char *p) {
+  for (int i = currEnd; i >= idx; i--) {
+    arr[i + 1] = arr[i];
+  }
+  currEnd++;
+  arr[idx] = p;
+}
+void pop() { currEnd--; }
+void erase(int idx) {
+  for (int i = idx; i < currEnd; i++) {
+    arr[i] = arr[i + 1];
+  }
+  currEnd--;
+}
 void printArray() {
+  printf("\n");
   for (int i = 0; i < currEnd; i++) {
     printf("Index: %3d Value: %s\n", i, arr[i]);
   }
 }
+
+char *get(int idx) { return arr[idx]; }
+
+int getLength() { return currEnd; }
--- array/array.h
+++ array/array.h
@@ -1,3 +1,10 @@
 void add(char *);
-char *get(int);
+void replace(int, char *);
+void insert(int, char *);
+void pop();
+void erase(int);
 void printArray();
+
+char *get(int);
+
+int getLength();
