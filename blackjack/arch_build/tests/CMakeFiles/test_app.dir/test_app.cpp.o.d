71dd0b73cd4ec8acb5d225e75a29e5f1f887f3ea
BxsedSosa <donovanxabad@gmail.com>
2026-01-31 04:38:52 -0800

    moved project

--- blackjack/arch_build/tests/CMakeFiles/test_app.dir/test_app.cpp.o.d
+++
@@ -1,3 +0,0 @@
-tests/CMakeFiles/test_app.dir/test_app.cpp.o: \
- /home/x/workspace/c-projects/blackjack/tests/test_app.cpp \
- /usr/include/stdc-predef.h
--- blackjack/src/Hand.cpp
+++
@@ -1,6 +0,0 @@
-#include "../includes/Hand.h"
-#include "../includes/Card.h"
-#include <vector>
-
-std::vector<Card> Hand::get_cards() { return hand; }
-void Hand::set_new_card(Card new_card) { hand.push_back(new_card); }
